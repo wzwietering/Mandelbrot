01b6dc171dabaf1509cc96ad057adeb612871d2c
Wilmer Zwietering <wzwietering@gmail.com>
2016-09-21 14:08:52 +0200

    Textbox changed to NumericUpDown. Added event handling to changes inside NUP.

--- Mandelbrot/Form1.cs
+++ Mandelbrot/Form1.cs
@@ -13,10 +13,10 @@ namespace Mandelbrot
             InitializeComponent();
             this.ImageDrawer = new ImageDrawer();
 
-            CreateControl(20, 20, ImageDrawer.MiddleX, "Middelste x:");
-            CreateControl(20, 50, ImageDrawer.MiddleY, "Middelste y:");
-            CreateControl(300, 20, ImageDrawer.Scale, "Schaal:");
-            CreateControl(300, 50, ImageDrawer.Max, "Max:");
+            CreateControl(20, 20, ImageDrawer.MiddleX, 0, "Middelste x:");
+            CreateControl(20, 50, ImageDrawer.MiddleY, 1, "Middelste y:");
+            CreateControl(300, 20, ImageDrawer.Scale, 2, "Schaal:");
+            CreateControl(300, 50, ImageDrawer.Max, 3, "Max:");
 
             var goButton = new Button() { Text = "Go!" };
             goButton.Click += CreateMandelbrotImage;
@@ -29,7 +29,7 @@ namespace Mandelbrot
             this.BackgroundImage = ImageDrawer.DrawImage(this.Height, this.Width);
         }
 
-        private void CreateControl(int x, int y, double value, string label)
+        private void CreateControl(int x, int y, double value, int id, string label)
         {
             Label Description = new Label();
             Description.Text = label;
@@ -37,17 +37,34 @@ namespace Mandelbrot
             this.Controls.Add(Description);
 
             //Nummerbox
-            TextBox ValueBox = new TextBox();
+            NumericUpDown ValueBox = new NumericUpDown();
             ValueBox.Location = new Point(Description.Width + 20 + x, y);
             ValueBox.Size = new Size(100, 20);
             ValueBox.Text = value.ToString();
+            ValueBox.DecimalPlaces = 3;
+            ValueBox.Name = id.ToString();
             this.Controls.Add(ValueBox);
             ValueBox.TextChanged += ValueChange;
         }
 
         private void ValueChange(object sender, EventArgs e)
         {
-
+            int id = Int32.Parse(((NumericUpDown)sender).Name);
+            switch (id)
+            {
+                case 0:
+                    ImageDrawer.MiddleX = (double)((NumericUpDown)sender).Value;
+                    break;
+                case 1:
+                    ImageDrawer.MiddleY = (double)((NumericUpDown)sender).Value;
+                    break;
+                case 2:
+                    ImageDrawer.Scale = (double)((NumericUpDown)sender).Value;
+                    break;
+                case 3:
+                    ImageDrawer.Max = (int)((NumericUpDown)sender).Value;
+                    break;
+            }
         }
     } 
 }
