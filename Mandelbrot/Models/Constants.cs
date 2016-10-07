58dd60251eb9ba66fb9b95628f12890e36816334
Wilmer Zwietering <wzwietering@gmail.com>
2016-10-07 12:51:18 +0200

    userGuideText has been updated to include all the awesome new features!!!!!!!!!!!!!

--- Mandelbrot/Models/Constants.cs
+++ Mandelbrot/Models/Constants.cs
@@ -18,13 +18,14 @@ beautiful images based on the mandelbrot formula!
 
 Try some preset images from the top menu > presets, or play around using the following hotkeys:
 
-left mouse button:     zoom in towards the spot you clicked.
-right mouse button:    zoom out towards te spot you clicked.
-Ctrl + minus:          zoom out
-Ctrl + Shift + plus:   zoom in
+Left mouse button:     zoom in towards the spot you clicked.
+Right mouse button:    zoom out towards te spot you clicked.
+Ctrl + minus:          zoom out.
+Ctrl + Shift + plus:   zoom in.
 Ctrl + arrow keys:     move around.
+Mousewheel:            zoom.
             
-You can change the color settings using the colorscheme dropdown, and you can also enter your own values into the boxes for X- and Y- coordinates, scale and max value. 
+You can change the color settings using the colorscheme dropdown, and you can also enter your own values into the boxes for x- and y-coordinates, scale and max value. 
 
 If you find a very nice image you can save it using the top menu > image > save.
 
